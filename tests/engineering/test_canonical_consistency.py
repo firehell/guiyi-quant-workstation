7e3a08554ba411764b2a247144f7c46b2cb5f9cc
zhangzhao <617620048@qq.com>
2026-08-25 14:40:50 +0800

    release: prepare v1.8.3

--- tests/engineering/test_canonical_consistency.py
+++ tests/engineering/test_canonical_consistency.py
@@ -255,7 +255,7 @@ def test_release_versions_are_consistent() -> None:
         web["version"],
     }
 
-    assert versions == {"1.8.2"}
+    assert versions == {"1.8.3"}
     assert "version=APP_VERSION" in api
     assert '"version": APP_VERSION' in api
 
